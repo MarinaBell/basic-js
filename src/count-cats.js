0aa64df6e69465246e71643b646b46959b365024
Marina Belenkaya <marina_vorobyeva1423@mail.ru>
2022-05-02 17:11:10 +0400

    feat: add fream team

--- src/count-cats.js
+++ src/count-cats.js
@@ -15,8 +15,8 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+  // throw new NotImplementedError('Not implemented');
+ 
 }
 
 module.exports = {
--- src/dream-team.js
+++ src/dream-team.js
@@ -13,10 +13,25 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+
+
+  function createDreamTeam(members) {
+
+    let arr = [];
+    if (Array.isArray(members)) {
+      for (let i = 0; i < members.length; i++) {
+        if(typeof(members[i]) == "string") {
+          members[i] =  members[i].replace(/\s+/g, '');
+          arr.push(members[i][0].toUpperCase());
+        }
+        }
+    } else {
+      return (false)
+    }
+
+      return (arr.sort().join(''));
+      
+    }
 
 module.exports = {
   createDreamTeam
